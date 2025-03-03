05d144d061aa13fe2e57da8181c761c875032241
AbulAlaJobayar <jobayar59@gmail.com>
2025-03-03 10:40:59 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['https://quickcash-server.vercel.app'],
+    origin: ['http://localhost:3000',"*"],
     credentials: true,
   }),
 );
